fc06181f4a7704efffc69675cab6934780643154
Olivia Osborn <olioz95@gmail.com>
2019-04-29 18:34:05 -0400

    filled in bubble sort

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -14,24 +14,37 @@ def selection_sort(arr):
             if arr[j] < arr[smallest_index]:
                 smallest_index = j
         # TO-DO: swap
-        placeholder = arr[i]
-        arr[i] = arr[smallest_index]
-        arr[smallest_index] = placeholder
+        arr[i], arr[smallest_index] = arr[smallest_index], arr[i]
         smallest_index = cur_index
     return arr
 
 
 print(selection_sort(nums))
+# O() = O(n^2)
 
 # TO-DO:  implement the Bubble Sort function below
 
 
 def bubble_sort(arr):
-
+    count = 0
+    cycle = True
+    while cycle:
+        for i in range(0, len(arr)-1):
+            if arr[i+1] < arr[i]:
+                arr[i], arr[i+1] = arr[i+1], arr[i]
+                count += 1
+        if count > 0:
+            cycle = True
+        else:
+            cycle = False
+        count = 0
     return arr
 
 
+print(bubble_sort(nums))
 # STRETCH: implement the Count Sort function below
+
+
 def count_sort(arr, maximum=-1):
 
     return arr
